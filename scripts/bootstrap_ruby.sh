4d390d2fe244325bc43248caf1cec1e2d6d9e631
Hossein Asgari <hossein1448@gmail.com>
2017-05-03 17:20:57 +0430

    change gpg keyserver url for avoiding get "gpg: keyserver receive failed: No route to host" error

--- scripts/bootstrap_ruby.sh
+++ scripts/bootstrap_ruby.sh
@@ -45,7 +45,7 @@ fi
 
 if [ -z $(which rvm) ]; then
   echo "${green}===== Requesting keys to install rvm… =====${reset}"
-  gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  gpg --keyserver wwwkeys.uk.pgp.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 
   echo "${green}===== Installing rvm… =====${reset}"
   \curl -sSL https://get.rvm.io | bash -s stable
